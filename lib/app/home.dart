345389a05fecbce65353a6b9a09fdfe4f6e6512f
Waoweens <30313089+Waoweens@users.noreply.github.com>
2023-11-24 13:27:32 +0700

    Add balance card to home page

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -1,3 +1,4 @@
+import 'package:cashalog/components/balance_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,10 +9,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int balance = 18651;
+
+  void deposit() {
+    debugPrint('Deposit');
+    setState(() {
+      balance++;
+    });
+  }
+
+  void withdraw() {
+    debugPrint('Withdraw');
+    setState(() {
+      balance--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text(
+            'Welcome back',
+            style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.w900),
+          ),
+          BalanceCard(
+            balance: balance,
+            deposit: deposit,
+            withdraw: withdraw,
+          )
+        ],
+      ),
     );
   }
 }
